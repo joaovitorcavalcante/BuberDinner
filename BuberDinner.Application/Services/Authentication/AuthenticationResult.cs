f8d44699a4aaa7fe1b4e4cc55253857b97ce1b8d
João <117310@pmenos.com.br>
2023-01-10 15:30:53 -0300

    Authentication Service in the Application Layer

---
+++ BuberDinner.Application/Services/Authentication/AuthenticationResult.cs
@@ -0,0 +1,7 @@
+﻿namespace BuberDinner.Application.Services.Authentication;
+public record AuthenticationResult(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Token);
---
+++ BuberDinner.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,6 @@
+﻿namespace BuberDinner.Application.Services.Authentication;
+public interface IAuthenticationService
+{
+    AuthenticationResult Login(string email, string password);
+    AuthenticationResult Register(string firstName, string lastName, string email, string password);
+}
